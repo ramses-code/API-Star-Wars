8e4d93fd77bb54b7b67ac5b5698766815fd6d8dc
Ramses Cevallos <cevallosramses@gmail.com>
2022-07-28 16:52:55 +0000

    created models and endpoints

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planets, Characters, Vehicles, Fav_planet, Fav_character, Fav_vehicle
 #from models import Person
 
 app = Flask(__name__)
@@ -31,12 +31,290 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def handle_user():
+
+    user = User.query.all()
+    user_serialized = list(map(lambda x: x.serialize(), user))
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        'results': user_serialized
     }
+    return jsonify(response_body), 200
 
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+
+    all_planets = Planets.query.all()
+    planets_serialized = list(map(lambda x: x.serialize(), all_planets))
+
+    response_body = {
+        'results': planets_serialized
+    }
+    return jsonify(response_body), 200
+
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def handle_planets_id(planets_id):
+
+    if planets_id < 1:
+        raise APIException('Planet id is not valid', status_code=400)
+
+    planet = Planets.query.get(planets_id)
+
+    if planet == None:
+        raise APIException('Planet does not exist', status_code=400)
+
+    response_body = {
+        'results': planet.serialize()
+    }
+    return jsonify(response_body), 200
+
+@app.route('/characters', methods=['GET'])
+def handle_characters():
+
+    all_characters = Characters.query.all()
+    characters_serialized = list(map(lambda x: x.serialize(), all_characters))
+
+    response_body = {
+        'results': characters_serialized
+    }
+    return jsonify(response_body), 200
+
+@app.route('/characters/<int:characters_id>', methods=['GET'])
+def handle_characters_id(characters_id):
+
+    if characters_id < 1:
+        raise APIException('Character id is not valid', status_code=400)
+
+    character = Characters.query.get(characters_id)
+
+    if character == None:
+        raise APIException('Character does not exist', status_code=400)
+
+    response_body = {
+        'results': character.serialize()
+    }
+    return jsonify(response_body), 200
+
+@app.route('/vehicles', methods=['GET'])
+def handle_vehicles():
+
+    all_vehicles = Vehicles.query.all()
+    vehicles_serialized = list(map(lambda x: x.serialize(), all_vehicles))
+
+    response_body = {
+        'results': vehicles_serialized
+    }
+    return jsonify(response_body), 200
+
+@app.route('/vehicles/<int:vehicles_id>', methods=['GET'])
+def handle_vehicles_id(vehicles_id):
+
+    if vehicles_id < 1:
+        raise APIException('Vehicle id is not valid', status_code=400)
+    
+    vehicle = Vehicles.query.get(vehicles_id)
+
+    if vehicle == None:
+        raise APIException('Vehicle does not exist', status_code=400)
+
+    response_body = {
+        'results': vehicle.serialize()
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/favorites/characters/<int:user_id>', methods=['GET'])
+def handle_fav_characters(user_id):
+
+    fav_all_characters = Characters.query.join\
+    (Fav_character, Fav_character.character_id == Characters.id)\
+    .filter(Fav_character.user_id == user_id)
+
+    all_characters = list(map(lambda x: x.serialize(), fav_all_characters))
+
+    response_body = {
+        'results': all_characters
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/favorites/planets/<int:user_id>', methods=['GET'])
+def handle_fav_planets(user_id):
+
+    fav_all_planets = Planets.query.join\
+    (Fav_planet, Fav_planet.planet_id == Planets.id)\
+    .filter(Fav_planet.user_id == user_id)
+
+    all_planets = list(map(lambda x: x.serialize(), fav_all_planets))
+
+    response_body = {
+        'results': all_planets
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/favorites/vehicles/<int:user_id>', methods=['GET'])
+def handle_fav_vehicles(user_id):
+
+    fav_all_vehicles = Vehicles.query.join\
+    (Fav_vehicle, Fav_vehicle.vehicle_id == Vehicles.id)\
+    .filter(Fav_vehicle.user_id == user_id)
+
+    all_vehicles = list(map(lambda x: x.serialize(), fav_all_vehicles))
+
+    response_body = {
+        'results': all_vehicles
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    body = request.get_json()
+
+    if body is None:
+        raise APIException\
+        ('You need to specify the request body as a json object', status_code=400)
+    if 'name' not in body:
+        raise APIException('Name field can not be empty', status_code=400)
+    if 'email' not in body:
+        raise APIException('Email field can not be empty', status_code=400)
+    if 'password' not in body:
+        raise APIException('Password field can not be empty', status_code=400)
+
+    new_user = User(name = body['name'], email = body['email'], \
+                    is_active = body['is_active'], password = body['password'])
+    db.session.add(new_user)
+    db.session.commit()
+
+    response_body = {
+        'results': new_user.serialize()
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/favorites/planets', methods=['POST'])
+def post_fav_planet():
+    body = request.get_json()
+
+    user_id = User.query.get(body['user_id'])
+    if user_id == None:
+        raise APIException('User does not exist', status_code=400)
+
+    fav_field = Fav_planet.query.filter_by\
+    (planet_id = body['planet_id']).filter_by(user_id = body['user_id']).all()
+    if len(fav_field) > 0:
+        raise APIException('User already liked this planet', status_code=400)
+
+    planet_id = Planets.query.get(body['planet_id'])
+    if planet_id == None:
+        raise APIException('Planet does not exist', status_code=400)
+
+    create_fav = Fav_planet(planet_id = body['planet_id'], user_id = body['user_id'])
+    db.session.add(create_fav)
+    db.session.commit()
+
+    response_body = {
+        'results': create_fav.serialize()
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/favorites/characters', methods=['POST'])
+def post_fav_character():
+    body = request.get_json()
+
+    user_id = User.query.get(body['user_id'])
+    if user_id == None:
+        raise APIException('User does not exist', status_code=400)
+
+    fav_field = Fav_character.query.filter_by\
+    (character_id = body['character_id']).filter_by(user_id = body['user_id']).all()
+    if len(fav_field) > 0:
+        raise APIException('User already liked this character', status_code=400)
+
+    character_id = Characters.query.get(body['character_id'])
+    if character_id == None:
+        raise APIException('Character does not exist', status_code=400)
+
+    create_fav = Fav_character(character_id = body['character_id'], user_id = body['user_id'])
+    db.session.add(create_fav)
+    db.session.commit()
+
+    response_body = {
+        'results': create_fav.serialize()
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/favorites/vehicles', methods=['POST'])
+def post_fav_vehicle():
+    body = request.get_json()
+
+    user_id = User.query.get(body['user_id'])
+    if user_id == None:
+        raise APIException('User does not exist', status_code=400)
+
+    fav_field = Fav_vehicle.query.filter_by\
+    (vehicle_id = body['vehicle_id']).filter_by(user_id = body['user_id']).all()
+    if len(fav_field) > 0:
+        raise APIException('User already liked this vehicle', status_code=400)
+
+    vehicle_id = Vehicles.query.get(body['vehicle_id'])
+    if vehicle_id == None:
+        raise APIException('Vehicle does not exist', status_code=400)
+
+    create_fav = Fav_vehicle(vehicle_id = body['vehicle_id'], user_id = body['user_id'])
+    db.session.add(create_fav)
+    db.session.commit()
+
+    response_body = {
+        'results': create_fav.serialize()
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/favorites/vehicles/<int:fav_id>/<int:user_id>', methods=['DELETE'])
+def delete_fav_vehicles(fav_id, user_id):
+
+    fav_v = Fav_vehicle.query.get(fav_id)
+    if fav_v == None:
+        raise APIException('Favorite does not exist', status_code=400)
+    
+    if fav_v.user_id != user_id:
+        raise APIException('User do not have this favorite', status_code=400)
+    db.session.delete(fav_v)
+    db.session.commit()
+
+    response_body = {
+        'results': 'ok'
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/favorites/planets/<int:fav_id>/<int:user_id>', methods=['DELETE'])
+def delete_fav_planets(fav_id, user_id):
+
+    fav_p = Fav_planet.query.get(fav_id)
+    if fav_p == None:
+        raise APIException('Favorite does not exist', status_code=400)
+
+    if fav_p.user_id != user_id:
+        raise APIException('User do not have this favorite', status_code=400)
+    db.session.delete(fav_p)
+    db.session.commit()
+
+    response_body = {
+        'results': 'ok'
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/favorites/characters/<int:fav_id>/<int:user_id>', methods=['DELETE'])
+def delete_fav_character(fav_id, user_id):
+
+    fav_c = Fav_character.query.get(fav_id)
+    if fav_c == None:
+        raise APIException('Favorite does not exist', status_code=400)
+
+    if fav_c.user_id != user_id:
+        raise APIException('User do not have this favorite', status_code=400)
+    db.session.delete(fav_c)
+    db.session.commit()
+
+    response_body = {
+        'results': 'ok'
+    }
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
--- src/models.py
+++ src/models.py
@@ -1,19 +1,118 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
+            'name': self.name,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    population = db.Column(db.Integer, unique=False, nullable=False)
+    
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'population': self.population,
+        }
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    mass = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'mass': self.mass,
+        }
+
+class Vehicles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    model = db.Column(db.String(20), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Vehicle %r>' % self.name
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'model': self.model,
+        }
+
+class Fav_character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id', ondelete='CASCADE'))
+    Characters = db.relationship('Characters', primaryjoin=character_id == Characters.id)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    User = db.relationship('User', primaryjoin=user_id == User.id)
+
+    def __repr__(self):
+        return '<Fav_character %r' % self.id
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'character_id': self.character_id,
+            'user_id': self.user_id
+        }
+
+class Fav_planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id', ondelete='CASCADE'))
+    Planets = db.relationship('Planets', primaryjoin=planet_id == Planets.id)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    User = db.relationship('User', primaryjoin=user_id == User.id)
+
+    def __repr__(self):
+        return '<Fav_planet %r' % self.id
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'planet_id': self.planet_id,
+            'user_id': self.user_id
+        }
+
+class Fav_vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.id', ondelete='CASCADE'))
+    Vehicles = db.relationship('Vehicles', primaryjoin=vehicle_id == Vehicles.id)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    User = db.relationship('User', primaryjoin=user_id == User.id)
+
+    def __repr__(self):
+        return '<Fav_vehicle %r' % self.id
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'vehicle_id': self.vehicle_id,
+            'user_id': self.user_id,
         }
